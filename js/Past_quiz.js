49bab32db03bcad835141558d95aed13d5d3206d
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2021-09-04 12:07:22 +0530

    update past quiz

--- js/Past_quiz.js
+++ js/Past_quiz.js
@@ -1,35 +1,20 @@
 let questions = [
-    {
-        numb: 1,
-        question: "What is past tense of \"Eat\"",
-        answer: "Ate",
-        options: [
-        "Ate",
-        "Eaten",
-        "All of the above",
-        "None of the above"
-    ]
-    },
-    {
-        numb: 2,
-        question: "What is past tense of \"Run\"",
-        answer: "Ran",
-        options: [
-        "Run",
-        "Ran",
-        "Raned",
-        "None of the above"
-        ]
-    },
-    {
-        numb: 3,
-        question: "He ___ the car yesterday.",
-        answer: "drove",
-        options: [
-        "drive",
-        "driven",
-        "drived",
-        "drove"
-        ]
-    }
+  {
+    numb: 1,
+    question: "My brother _____ a bear an hour ago.",
+    answer: "saw",
+    options: ["seen", "saw", "sees", "None of the above"],
+  },
+  {
+    numb: 2,
+    question: " _____ Mike visit his grandmother last night?",
+    answer: "Did",
+    options: ["Did", "Are", "Does", "None of the above"],
+  },
+  {
+    numb: 3,
+    question: "_____ Judy and Liz at last month's meeting?",
+    answer: "Were",
+    options: ["Was", "Were", "Are", "Is"],
+  },
 ];
