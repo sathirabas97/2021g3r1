50a00bd2dee19d3b8af0e1ace40f7045ea7d90ea
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2021-09-04 12:07:11 +0530

    quiz present bug fix

--- js/Present_quiz.js
+++ js/Present_quiz.js
@@ -1,57 +1,32 @@
 let questions = [
-    {
-        numb: 1,
-        question: "What is present tense of \"Ate\"",
-        answer: "Eat",
-        options: [
-        "Eat",
-        "Eaten",
-        "All of the above",
-        "None of the above"
-    ]
-    }
-    {
-        numb: 2,
-        question: "Do you __________ chocolate milk?",
-        answer: "like",
-        options: [
-        "like",
-        "likes",
-        "Be like",
-        "None of the above"
-    ]
-    }
-    {
-        numb: 3,
-        question: "He _________ not want to go to movies",
-        answer: "does",
-        options: [
-        "do",
-        "does",
-        "did",
-        "None of the above"
-    ]
-    }
-    {
-        numb: 1,
-        question: "It ____________ beautiful day today",
-        answer: "is",
-        options: [
-        "am",
-        "is",
-        "are",
-        "None of the above"
-    ]
-    }
-    {
-        numb: 1,
-        question: "My parents lives in a bedroom apartment",
-        answer: "live",
-        options: [
-        "live",
-        "lives",
-        "are live",
-        "None of the above"
-    ]
-    }
+  {
+    numb: 1,
+    question: 'What is present tense of "Ate"',
+    answer: "Eat",
+    options: ["Eat", "Eaten", "All of the above", "None of the above"],
+  },
+  {
+    numb: 2,
+    question: "Do you __________ chocolate milk?",
+    answer: "like",
+    options: ["like", "likes", "Be like", "None of the above"],
+  },
+  {
+    numb: 3,
+    question: "He _________ not want to go to movies",
+    answer: "does",
+    options: ["do", "does", "did", "None of the above"],
+  },
+  {
+    numb: 4,
+    question: "It ____________ beautiful day today",
+    answer: "is",
+    options: ["am", "is", "are", "None of the above"],
+  },
+  {
+    numb: 5,
+    question: "My parents lives in a bedroom apartment",
+    answer: "live",
+    options: ["live", "lives", "are live", "None of the above"],
+  },
 ];
