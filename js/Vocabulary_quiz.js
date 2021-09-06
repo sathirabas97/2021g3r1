15a8019284f071f02af0def0408634699271ac02
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2021-09-06 19:00:50 +0530

    vocabulary quiz questions added

--- js/Vocabulary_quiz.js
+++ js/Vocabulary_quiz.js
@@ -1,19 +1,19 @@
 let questions = [
   {
     numb: 1,
-    question: "Fill in the blanks" + "H_LLO",
+    question: "Fill in the blanks" + ": H_LLO",
     answer: "E",
     options: ["E", "O", "I", "G"],
   },
   {
     numb: 1,
-    question: "Fill in the blanks" + "G_EST",
+    question: "Fill in the blanks" + ": G_EST",
     answer: "U",
     options: ["E", "O", "U", "G"],
   },
   {
     numb: 1,
-    question: "Fill in the blanks" + "ELEP_ANT",
+    question: "Fill in the blanks" + ": ELEP_ANT",
     answer: "H",
     options: ["E", "O", "I", "H"],
   },
